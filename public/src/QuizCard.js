a313b55378b3ca81d39b1bc8955f346b7fb7152c
debashisjena100 <debashisjena100@gmail.com>
2025-12-05 10:38:27 +0000

    styles added

--- public/src/QuizCard.js
+++ public/src/QuizCard.js
@@ -3,6 +3,7 @@ import React from 'react';
 const QuizCard = ({ quizName }) => {
   return (
     <div className="quiz-card">
+      <div className="quiz-icon">▶</div>
       <h2 className="quiz-card-title">{quizName}</h2>
     </div>
   );
