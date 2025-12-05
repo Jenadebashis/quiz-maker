25e8b79392bc7a9484f5fbc8c0163ce72bff3ddf
debashisjena100 <debashisjena100@gmail.com>
2025-12-05 10:29:59 +0000

    try 2

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const path = require('path');
 module.exports = {
   entry: './public/src/index.js',
   output: {
-    path: path.resolve(__dirname, 'public/dist'),
+    path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
     publicPath: '/dist/'
   },
